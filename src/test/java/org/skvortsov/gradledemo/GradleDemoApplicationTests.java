bc6415fa857b0ceb42b6d2f37cf49ea3f4baa8d4
fedorskvortsov <skvortsovfk@gmail.com>
2022-03-14 15:09:15 +0300

    initial commit

---
+++ src/test/java/org/skvortsov/gradledemo/GradleDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.skvortsov.gradledemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradleDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
